e93a68760a58466be4b9a9c80916662b9726a38d
Mr. Mogoba <mogobakamo8@gmail.com>
2026-08-19 21:12:40 +0200

    Add Terraform configuration for Azure resources

---
+++ terraform/main.tf
@@ -0,0 +1,77 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+# 1. Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-${var.project_name}-${var.environment}"
+  location = var.location
+  tags = {
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
+# 2. Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-${var.project_name}-${var.environment}"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+# 3. Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-${var.project_name}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+# 4. Public IP for the VM
+resource "azurerm_public_ip" "pip" {
+  name                = "pip-${var.project_name}-vm"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+# 5. Network Interface
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-${var.project_name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+# 6. Linux Virtual Machine (e.g., for running Docker / Kafka / Python consumers)
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = "vm-${var.project_name}"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = var.vm_size
+  admin_username        = "azureuser"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
